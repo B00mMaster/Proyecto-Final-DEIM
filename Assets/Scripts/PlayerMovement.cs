5df6d2e3663b1efcbd24cf6743676109e92a6f85
B00mMaster <pablogaudesi@gmail.com>
2024-01-14 22:57:44 +0100

    .

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,46 +9,101 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
+    public int jumpsLeft;
+    private bool isGrounded;
+    private enum movement { iddle,running,jumping,falling};
+   
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        jumpsLeft = 2;
     }
     void Update()
     {
-
+        if (jumpsLeft == 0 && isGrounded == true)
+        {
+            jumpsLeft = 2;
+        }
+        movement state;
         
         horizontalInput = Input.GetAxis("Horizontal");
 
         
         rb.velocity= new Vector2 (horizontalInput*speed,rb.velocity.y);
 
-        if(Input.GetKeyDown("up"))
+        if(Input.GetKeyDown("up")&&(isGrounded&&jumpsLeft>0))
         {
-           
-            rb.velocity=new Vector3(0,10,0);
-            anim.SetBool("jumping", true);
-            anim.SetBool("running", false);
+            Jump();
         }
 
-        if(horizontalInput >0) 
+
+        if(horizontalInput >0f) 
         {
-            anim.SetBool("jumping", false);
-            anim.SetBool("running", true);
+            state = movement.running;
             sprite.flipX = false;
         }
-        else if (horizontalInput < 0)
+        else if (horizontalInput < 0f)
         {
-            anim.SetBool("jumping", false);
-            anim.SetBool("running", true);
+            state = movement.running;
             sprite.flipX=true;
         }
         else  
         {
-            anim.SetBool("jumping", false);
-            anim.SetBool("running", false);
+            state = movement.iddle;
         }
+
+        if(rb.velocity.y>.1f)
+        {
+            state = movement.jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = movement.falling;
+        }
+
+        anim.SetInteger("state",(int)state);
     }
+
+    void Jump()
+    {
+        rb.velocity = new Vector3(0, 12, 0);
+        jumpsLeft--;
+
+        if (!isGrounded)
+        {
+            jumpsLeft = 2;
+        }
+        if(jumpsLeft==0)
+        { 
+            isGrounded = false;
+        
+        }
+        
+    }
+    public Transform respawn;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true;
+            jumpsLeft = 2;
+
+
+        }
+
+        if (collision.gameObject.CompareTag("end"))
+        {
+            Destroy(gameObject);
+
+            Instantiate(gameObject, respawn.position, Quaternion.identity);
+        }
+
+
+    }
+
+
 }
